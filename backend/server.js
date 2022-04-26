993d6b792cf818e93d60e30f0985694a539cab25
daradeepak <deepak.dara07@gmail.com>
2022-04-27 01:19:06 +0530

    Multer Done

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import fsr from 'file-stream-rotator'
 import FileStreamRotator from 'file-stream-rotator'
-// import uploadRoutes from './routes/uploadRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 dotenv.config()
 
 connectDB()
@@ -33,9 +33,9 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-app.use("/api-docs",
-  swaggerUi.serve,swaggerUi.setup(swaggerDocument))
-// app.use('/api/upload', uploadRoutes)
+app.use('/api/upload', uploadRoutes)
+app.use("/api-docs",  swaggerUi.serve,swaggerUi.setup(swaggerDocument))
+app.use('/api/upload', uploadRoutes)
 
 // app.get('/api/config/paypal', (req, res) =>
 //   res.send(process.env.PAYPAL_CLIENT_ID)
